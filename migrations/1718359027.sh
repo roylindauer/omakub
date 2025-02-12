9e008bf04924404a2276805c148d4f46005038e4
Roy Lindauer <1851879+roylindauer@users.noreply.github.com>
2025-02-12 08:58:03 -0800

    Removes apps I do not use or want

--- migrations/1718359027.sh
+++ migrations/1718359027.sh
@@ -5,16 +5,16 @@ sudo echo "Running upgrade migration..."
 # Change Zellij directory to be realized rather than a symlink
 ZELLIJ_THEMES_DIR="$HOME/.config/zellij/themes"
 if [ -L $ZELLIJ_THEMES_DIR ]; then
-	rm $ZELLIJ_THEMES_DIR
-	mkdir -p $ZELLIJ_THEMES_DIR
-	for dir in $OMAKUB_PATH/themes/*; do
-		if [ -d "$dir" ]; then
-			zellij_file="$dir/zellij.kdl"
-			dir_name=$(basename "$dir")
-			dest_file="$ZELLIJ_THEMES_DIR/$dir_name.kdl"
-			cp "$zellij_file" "$dest_file"
-		fi
-	done
+  rm $ZELLIJ_THEMES_DIR
+  mkdir -p $ZELLIJ_THEMES_DIR
+  for dir in $OMAKUB_PATH/themes/*; do
+    if [ -d "$dir" ]; then
+      zellij_file="$dir/zellij.kdl"
+      dir_name=$(basename "$dir")
+      dest_file="$ZELLIJ_THEMES_DIR/$dir_name.kdl"
+      cp "$zellij_file" "$dest_file"
+    fi
+  done
 fi
 
 # New neovim settings
@@ -50,8 +50,6 @@ source $OMAKUB_PATH/applications/Activity.sh
 source $OMAKUB_PATH/applications/Docker.sh
 
 # Update icons of apps still installed
-[ -f ~/.local/share/applications/Basecamp.desktop ] && source $OMAKUB_PATH/applications/Basecamp.sh
-[ -f ~/.local/share/applications/HEY.desktop ] && source $OMAKUB_PATH/applications/HEY.sh
 [ -f ~/.local/share/applications/WhatsApp.desktop ] && source $OMAKUB_PATH/applications/WhatsApp.sh
 
 # Set new app grid
@@ -61,11 +59,11 @@ source $OMAKUB_PATH/install/desktop/set-app-grid.sh
 source $OMAKUB_PATH/install/desktop/set-gnome-extensions.sh
 
 gum style \
-	--foreground 212 --border-foreground 212 --border double \
-	--align left --width 80 --margin "1 2" --padding "2 4" \
-	"1. alacritty.toml config moved to .bak to include new font-size.toml" \
-	"2. Alacritty theme/font has been reset. Use omakub app to set again." \
-	"3. To use Pano, the new clipboard manager, enable in Gnome Extensions."
+  --foreground 212 --border-foreground 212 --border double \
+  --align left --width 80 --margin "1 2" --padding "2 4" \
+  "1. alacritty.toml config moved to .bak to include new font-size.toml" \
+  "2. Alacritty theme/font has been reset. Use omakub app to set again." \
+  "3. To use Pano, the new clipboard manager, enable in Gnome Extensions."
 
 gum confirm "Set your application dock to default with new apps?" && source $OMAKUB_PATH/install/desktop/set-dock.sh
 gum confirm "Ready to logout for all settings to take effect?" && gnome-session-quit --logout --no-prompt
